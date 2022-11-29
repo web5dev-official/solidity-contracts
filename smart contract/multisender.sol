088d99c2ac7dc332d8f802baed05e2969635f39c
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2022-11-29 12:11:25 +0530

    Update multisender.sol

--- smart contract/multisender.sol
+++ smart contract/multisender.sol
@@ -2,19 +2,18 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-  
     event Transfer(address indexed from, address indexed to, uint256 value);
     function transfer(address to, uint256 amount) external returns (bool);
 }
 contract multisender {
-   uint256 public amount = 10*10**18;
+   uint256 public amount = 10**18;
    IERC20 usdt = IERC20(
-            address(0x0Af7aeE626B2641cb1c91c4D42F903D37D88148F)
+            address(0x3EEE5653664248E160eb379Cfdae26da1bfa042E)
         );
        
-    function sendTo (address[] memory addrs) public {
+    function sendTo (address[] memory addrs, uint256[] memory _amount) public {
     for(uint i = 0; i < addrs.length; i++) {
-        usdt.transfer(addrs[i],10);
+        usdt.transfer(addrs[i], _amount[i]*amount);
     }
    }
 }
