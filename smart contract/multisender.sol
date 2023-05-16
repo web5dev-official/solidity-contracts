6e4253495cd8e43f53475dbb4a20b95731bb727c
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2023-05-16 17:15:16 +0530

    fixed solidity error

--- smart contract/multisender.sol
+++ smart contract/multisender.sol
@@ -3,45 +3,56 @@ pragma solidity ^0.8.0;
 
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
+
     function balanceOf(address account) external view returns (uint256);
+
     function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
 }
 
 contract Multisender {
-    event TokensSent(address indexed token, address indexed from, uint256 totalAmount);
-    event TokensTransferred(address indexed token, address indexed to, uint256 amount);
-
-    function sendTo(address token, uint8 decimals, address[] memory recipients, uint256[] memory amounts) public payable {
+    event TokensSent(
+        address indexed token,
+        address indexed from,
+        uint256 totalAmount
+    );
+    event TokensTransferred(
+        address indexed token,
+        address indexed to,
+        uint256 amount
+    );
+  
+    function sendTo(
+        address token,
+        uint8 decimals,
+        address[] memory recipients,
+        uint256[] memory amounts
+    ) public {
         require(recipients.length == amounts.length, "Invalid input");
 
-        if (token == address(0)) {
-            require(msg.value == totalAmount(amounts), "Incorrect amount sent");
+        IERC20 erc20 = IERC20(token);
+        erc20.transferFrom(msg.sender, address(this), totalAmount(amounts)*10**decimals);
 
-            for (uint256 i = 0; i < recipients.length; i++) {
-                payable(recipients[i]).transfer(amounts[i]);
-                emit TokensTransferred(token, recipients[i], amounts[i]);
-            }
-        } else {
-            IERC20 erc20 = IERC20(token);
-            erc20.transferFrom(msg.sender, address(this), totalAmount(amounts));
-
-            for (uint256 i = 0; i < recipients.length; i++) {
-                safeTransfer(erc20, recipients[i], amounts[i], decimals);
-            }
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(erc20.transfer(recipients[i], amounts[i]*10**decimals), "Transfer failed");
+            emit TokensTransferred(token, recipients[i], amounts[i]);
         }
 
         emit TokensSent(token, msg.sender, totalAmount(amounts));
     }
 
-    function safeTransfer(IERC20 token, address to, uint256 value, uint8 decimals) private {
-        uint256 adjustedValue = value * (10 ** uint256(decimals));
-        require(token.balanceOf(address(this)) >= adjustedValue, "Insufficient balance");
-        require(token.transfer(to, adjustedValue), "Transfer failed");
-        emit TokensTransferred(address(token), to, value);
-    }
-
-    function totalAmount(uint256[] memory amounts) private pure returns (uint256) {
+    
+    function totalAmount(uint256[] memory amounts)
+        private
+        pure
+        returns (uint256)
+    {
         uint256 total = 0;
 
         for (uint256 i = 0; i < amounts.length; i++) {
