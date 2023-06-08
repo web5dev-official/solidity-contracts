12b454b4907aa6c8b23104032b80bc25c25dda07
WEB5LAB <88079157+web5lab@users.noreply.github.com>
2023-06-08 13:29:46 +0530

    updated code

--- icofactory.sol
+++ icofactory.sol
@@ -7,29 +7,32 @@ contract ICOFactory {
     event ICOContractDeployed(address indexed icoContract, address indexed tokenAddress, uint256 amount);
 
     mapping(address => address) public deployedICOContracts;
+    
+ function createICOContract(
+   IcoData memory _icoData, IcoUrl memory _url ,address _tokenAddress , uint256 _amount
+) public {
+    // Assume the ERC20 token contract has a transferFrom function
+    IERC20 token = IERC20(_tokenAddress);
 
-    function createICOContract(
-        address _tokenAddress,
-        uint256 _amount,
-        uint256 _startTimestamp,
-        uint256 _endTimestamp,
-        uint256 _refralRate,
-        string memory _website,
-        string memory _selectedCurrency
-    ) public {
-        ico_contract ico = new ico_contract(
-            _tokenAddress,
-            _amount,
-            _startTimestamp,
-            _endTimestamp,
-            _refralRate,
-            _website,
-            _selectedCurrency
-        );
+    // Check if the caller has approved the ICOFactory contract to transfer tokens on their behalf
+    require(
+        token.transferFrom(msg.sender, address(this), _amount),
+        "contract deployment failed"
+    );
 
-        deployedICOContracts[_tokenAddress] = address(ico);
-        emit ICOContractDeployed(address(ico), _tokenAddress, _amount);
-    }
+    ico_contract ico = new ico_contract(
+        _icoData , _url
+    );
+
+    // Transfer the tokens from ICOFactory contract to the deployed ICO contract
+    require(
+        token.transfer(address(ico), _amount),
+        "Token transfer to ICO contract failed."
+    );
+
+    deployedICOContracts[_tokenAddress] = address(ico);
+    emit ICOContractDeployed(address(ico), _tokenAddress, _amount);
+}
 
     function getDeployedICOContract(address _tokenAddress) public view returns (address) {
         return deployedICOContracts[_tokenAddress];
